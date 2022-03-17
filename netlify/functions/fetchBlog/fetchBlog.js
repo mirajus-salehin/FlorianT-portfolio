655b230b4b029502c5348b55b77acf3d8010de40
Sayem <mirazussalehin@outlook.com>
2022-03-17 16:43:02 +0600

    added netlify configatration

--- netlify/functions/fetchBlog/fetchBlog.js
+++ netlify/functions/fetchBlog/fetchBlog.js
@@ -1,17 +1,22 @@
 // Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
-const handler = async (event) => {
-  try {
-    const subject = event.queryStringParameters.name || 'World'
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: `Hello ${subject}` }),
-      // // more keys you can return:
-      // headers: { "headerName": "headerValue", ... },
-      // isBase64Encoded: true,
-    }
-  } catch (error) {
-    return { statusCode: 500, body: error.toString() }
-  }
-}
+const {
+  Client
+} = require("@notionhq/client")
 
-module.exports = { handler }
+// Initializing a client
+const notion = new Client({
+  auth: process.env.NOTION_API_KEY,
+})
+
+exports.handler = async function (event, context) {
+  const response = await notion.databases.query({
+      database_id: process.env.NOTION_DB_ID,
+  })
+  console.log(response)
+  return {
+      statusCode: 200,
+      body: JSON.stringify({
+          response
+      }),
+  };
+}
