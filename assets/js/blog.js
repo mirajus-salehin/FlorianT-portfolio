56b451865f03640764ee998ca4103577b7b2d20e
Sayem <mirazussalehin@outlook.com>
2022-03-17 16:33:39 +0600

    working with serverless blog

---
+++ assets/js/blog.js
@@ -0,0 +1,21 @@
+const {
+    Client
+} = require("@notionhq/client")
+
+// Initializing a client
+const notion = new Client({
+    auth: process.env.NOTION_API_KEY,
+})
+
+exports.handler = async function (event, context) {
+    const response = await notion.databases.query({
+        database_id: process.env.NOTION_DB_ID,
+    })
+    console.log(response)
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+            response
+        }),
+    };
+}
